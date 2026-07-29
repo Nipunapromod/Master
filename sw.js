2fe66b1193196fd38762292db87d68d633f5d580
Nipuna pramod <104716513+Nipunapromod@users.noreply.github.com>
2026-07-29 15:28:24 +0530

    mai

---
+++ sw.js
@@ -0,0 +1,35 @@
+const cacheName = 'mastermind-ai-pwa-v1';
+const assetsToCache = [
+  '.',
+  'index.html',
+  'logo.ico',
+  'manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => cache.addAll(assetsToCache))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cacheResponse) => {
+      return cacheResponse || fetch(event.request);
+    })
+  );
+});
